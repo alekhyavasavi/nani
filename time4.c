2224b2c9fac10344a896f6e8d4a333fc1dae7753
alekhyavasavi <42729355+alekhyavasavi@users.noreply.github.com>
2018-09-07 12:57:07 +0530

    Create time4.c

---
+++ time4.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f;
+    printf("");
+    scanf("%d %d",&a,&c);
+    printf("");
+    scanf("%d %d",&b,&d);
+    e=a-b;
+    f=c-d;
+    printf("%d %d",e,f);
+    return 0;
+}
